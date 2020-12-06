45c993d58857fbb6202ebd7d3add0e814f286a30
Devika Pagare <65743242+devika-be@users.noreply.github.com>
2020-12-06 12:14:09 +0530

    Create Hello World.js

---
+++ Hello World.js
@@ -0,0 +1,11 @@
+# Problem link: https://www.hackerrank.com/challenges/js10-hello-world/problem
+
+# ANS:
+
+function greeting(parameterVariable) {
+    // This line prints 'Hello, World!' to the console:
+    console.log('Hello, World!');
+
+    // Write a lin
+    console.log(parameterVariable);
+}
