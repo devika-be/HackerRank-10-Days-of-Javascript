134cefa1f5f6946c7f6f69dda5ce1fd889d0f805
Devika Pagare <65743242+devika-be@users.noreply.github.com>
2021-01-11 15:05:12 +0530

    Create Day 1: Arithmetic Operators.js

---
+++ Day 1: Arithmetic Operators.js
@@ -0,0 +1,20 @@
+#Problem Link : https://www.hackerrank.com/challenges/js10-arithmetic-operators/problem
+
+#Ans :
+function getArea(length, width) {
+    let area;
+    // Write your code here
+    area = length * width;
+    
+    return area;
+}
+
+function getPerimeter(length, width) {
+    let perimeter;
+    // Write your code here
+    perimeter = 2 * (length + width);
+    
+    return perimeter;
+}
+
+
