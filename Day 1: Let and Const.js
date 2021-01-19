e35bd85e6c91584dc9c58de10a829edc182f3e12
Devika Pagare <65743242+devika-be@users.noreply.github.com>
2021-01-19 19:18:13 +0530

    Create Day 1: Let and Const.js

---
+++ Day 1: Let and Const.js
@@ -0,0 +1,13 @@
+#problem Link : https://www.hackerrank.com/challenges/js10-let-and-const/problem
+
+#Ans : 
+
+function main() {
+   const PI = Math.PI;
+    const r = +(readLine());
+    
+    // Print the area of the circle:
+    console.log(PI * r * r);
+    
+    // Print the perimeter of the circle:
+    console.log(2 * PI * r);
