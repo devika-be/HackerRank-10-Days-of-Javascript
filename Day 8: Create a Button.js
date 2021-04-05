496ed443d10c10c2a25ea953cc87b983225fd96a
Devika Pagare <65743242+devika-be@users.noreply.github.com>
2021-04-05 21:23:27 +0530

    Update Day 8: Create a Button.js

--- Day 8: Create a Button.js
+++ Day 8: Create a Button.js
@@ -1,1 +1,60 @@
+#Problem Link : https://www.hackerrank.com/challenges/js10-create-a-button/problem
 
+#Ans :
+
+#(1)CSS file :
+
+.btnClass {
+
+    width: 96px;
+
+    height: 48px;
+
+    font-size: 24px;
+
+}
+
+
+
+#(2)JavaScript file :
+
+var btn = document.createElement("Button");
+
+btn.innerHTML = "0";
+
+btn.id = "btn";
+
+btn.className = "btnClass";
+
+
+
+document.body.appendChild(btn);
+
+
+
+btn.onclick = function() {
+
+    btn.innerHTML+=2;
+
+}
+
+
+#(3)HTML file :
+
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+
+        <meta charset="utf-8">
+
+        <title>Button</title>
+
+    </head>
+
+    <body>
+
+    </body>
+
+</html>
